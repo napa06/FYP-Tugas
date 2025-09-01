1f1ac07a04010fbca53078a725ba5477a53043cf
Dev Karel Christanto <davkarelchristanto@gmail.com>
2025-09-01 18:22:52 +0700

    Menambahkan file C pertama (main.c)

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int x = halo;
+    scanf("%c", x);
+    printf("Katakan Halo = %c\n",x);
+    printf("Hello, Saya Dev Karel Christanto\n");
+    printf("Bahasa Pemograman kesukaan saya sekarang adalah bahasa C\n");
+    printf("Karena itu bahasa pertama yang saya pelajari di FYP ini\n");
+    return 0;
+}
