e2fdc6ebc491d40a3c930427f68fbb25719e2b78
Dev Karel Christanto <davkarelchristanto@gmail.com>
2025-09-01 18:38:48 +0700

    Update main.c menjadi program pengecek Halo

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
-    int x = halo;
-    scanf("%c", x);
-    printf("Katakan Halo = %c\n",x);
+    char x[10];
+    printf("Katakan Halo: \n");
+    scanf("%s", x);
+
+if (strcmp(x, "halo") == 0) {
     printf("Hello, Saya Dev Karel Christanto\n");
     printf("Bahasa Pemograman kesukaan saya sekarang adalah bahasa C\n");
     printf("Karena itu bahasa pertama yang saya pelajari di FYP ini\n");
+    } else {
+    printf("Halo dulu dong\n");
+    }
     return 0;
 }
